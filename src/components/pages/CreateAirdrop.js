608a513354e68ca94e8f812d2524e88d52b0308d
manuochoa <manu_8a@outlook.com>
2022-04-08 16:11:52 +0200

    last commit

--- src/components/pages/CreateAirdrop.js
+++ src/components/pages/CreateAirdrop.js
@@ -1,11 +1,48 @@
-import React from 'react';
-import AddTokenAddress from '../common/AddTokenAddress';
+import React, { useState } from "react";
+import AddTokenAddress from "../common/AddTokenAddress";
 
 export default function CreateAirdrop() {
-    return (
-        <div className="container">
-            <h1 className="title title--page">Create Airdrop</h1>
-            <AddTokenAddress inputInfo="Create airdrop fee: 0.5 BNB" />
-        </div>
-    );
+  const [errors, setErrors] = useState([]);
+  const [launchDetails, setLaunchDetails] = useState({
+    price: "",
+    listingPrice: "",
+    whitelist: false,
+    softCap: "",
+    hardCap: "",
+    minBuy: "",
+    maxBuy: "",
+    startTime: "",
+    endTime: "",
+    lockupPeriod: "",
+    liquidityPerc: "",
+    tokenAddress: "",
+    tokenDecimals: "",
+    tokenName: "",
+    tokenSymbol: "",
+    isAllowed: false,
+    infoURL: "",
+  });
+  const [extraInfo, setExtraInfo] = useState({
+    logo: "",
+    website: "",
+    facebook: "",
+    twitter: "",
+    github: "",
+    telegram: "",
+    instagram: "",
+    discord: "",
+    reddit: "",
+    description: "",
+  });
+  return (
+    <div className="container">
+      <h1 className="title title--page">Create Airdrop</h1>
+      <AddTokenAddress
+        errors={errors}
+        launchDetails={launchDetails}
+        setLaunchDetails={setLaunchDetails}
+        inputInfo="Create airdrop fee: 0.5 BNB"
+      />
+    </div>
+  );
 }
